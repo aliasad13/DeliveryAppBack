96f324661c0d9c8796756a4926d58fb6486c74b5
ali <ali.shah@foradian.com>
2024-08-03 20:19:27 +0530

    modal added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :user_params, only: [:update_full_name]
+  before_action :set_user, only: [:update_full_name]
   def user_details
     if request.headers.present? and request.headers['Authorization']
     header = request.headers['Authorization']
@@ -29,4 +32,37 @@ class UsersController < ApplicationController
       render json: { errors: "Token Missing" }, status: :unauthorized
     end
   end
+
+  def update_names
+    if request.patch? and @user
+        if @user.update(user_params)
+          render json: { success: "Profile updated" }, status: :ok
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+  end
+
+  def send_mail_verification_otp
+    render json: { success: "otp send" }, status: :ok
+  end
+
+  def update_email
+
+  end
+
+  def update_password
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,14 @@ Rails.application.routes.draw do
   get 'user', to: 'users#user_details'
 
   #patch
-
-
+  resources :users, only: [] do
+    member do
+      patch 'update_names'
+      post 'send_mail_verification_otp'
+      patch 'update_email'
+      patch 'update_password'
+    end
+  end
   #delete
 
 
